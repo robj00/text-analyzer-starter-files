eeb2d37b762f614028f7d46ef0dd7b77029f6bdf
Rob Palmer <rjpalmer15@gmail.com>
2017-05-11 15:09:43 -0400

    Project complete

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 'use strict';
 function submitHandler () {
-	$('.js-submit').click(function() {
+	$('.js-submit').click(function(event) {
+		event.preventDefault();
 		var textBlock = $('.js-textInput').val();
-		console.log(textBlock);
-		var wordArray = textBlock.toLowerCase().split(/[ ,.?!:;-"]+/).filter(Boolean).sort();
-		console.log(wordArray);
-		var charCount;
-		for (var i = 1; i < wordArray.length ; i++) {
+		var wordArray = textBlock.toLowerCase().split(/[ -,.?!:;"]+/).filter(Boolean).sort();
+		var charCount = 0;
+		for (var i = 1; i <= wordArray.length ; i++) {
 			charCount = charCount + wordArray[i-1].length;
-			if ( i = 1 ) { 
+			if ( i == 1 ) { 
 			var uniqueWords = 1;
 			}else if (wordArray[i-1] != wordArray[i-2]) {
 				uniqueWords ++
 			}
 		}
+
+		var avgLength = charCount/wordArray.length
 		$('.text-report').removeClass('hidden');
 		$('.js-wordCount').text(wordArray.length);	
 		$('.js-uniqueCount').text(uniqueWords);	
-		$('.js-acgLength').text(charCount/wordArray.length + ' characters');
+		$('.js-avgLength').text(avgLength + ' characters');
 
 	});
 }
